b23e9a0fcc7793c4a71b7063260d965b67fd14ce
TalhaKocc <talhakocc1834@gmail.com>
2026-01-18 19:16:20 +0300

    çalışan güncelleme işlemi için dto class'ı oluşturuldu

--- src/main/java/com/talhakoc/dto/employee/request/EmployeeUpdateDto.java
+++ src/main/java/com/talhakoc/dto/employee/request/EmployeeUpdateDto.java
@@ -1,4 +1,31 @@
 package com.talhakoc.dto.employee.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeUpdateDto {
+
+    @Email(message = "Lütfen email formatında giriniz")
+    @NotBlank(message = "Bu alan boş bırakılamaz")
+    private String email;
+
+    private String password;
+
+    @NotBlank(message = "Bu alan boş bırakılamaz")
+    private String fullName;
+
+    @NotBlank(message = "Lütfen telefon numarası giriniz")
+    @Pattern(regexp = "^[0-9]{10,11}$", message = "Telefon numarası 10-11 hane olmalıdır")
+    private String phoneNumber;
+
+
 }
